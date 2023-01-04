9094b397978802bcf92e617202dc2089f290c193
Kirill <18kirilllarin@gmail.com>
2023-01-04 20:27:07 +0700

    add new files

---
+++ 123.py
@@ -0,0 +1,10 @@
+import hashlib
+
+filename = input("Укажите файл: ")
+file = open(filename, 'r')
+hash = open(hash.txt, 'w')
+
+
+
+file.close()
+hash.close()
---
+++ examples.py
@@ -0,0 +1,12 @@
+import hashlib  # импортирование модуля
+
+string = input('Введите строку для хэширования: ')  # ввод пользователем строки
+
+# преобразование текстовой строки в строку байтов и перевод её в хэш с помощью MD5 и SHA256
+hash_object_md5 = hashlib.md5(string.encode())
+hash_object_sha256 = hashlib.sha256(string.encode())
+
+# вывод хэша
+print("Хэш Вашей строки, полученный с помощью MD5: ", hash_object_md5.hexdigest())
+print("Хэш Вашей строки, полученный с помощью SHA256: ", hash_object_sha256.hexdigest())
+
